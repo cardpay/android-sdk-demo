01abb27998f0499c0c55c001b01eb81c34d14b2d
a.potapov <a.potapov@unlimint.com>
2021-05-12 18:55:01 +0300

    MDA-402 add Paypal and token payment

--- app/src/main/java/com/unlimint/view/PaymentWithTokenFragment.kt
+++ app/src/main/java/com/unlimint/view/PaymentWithTokenFragment.kt
@@ -82,8 +82,6 @@ class PaymentWithTokenFragment : Fragment() {
             }
         })
 
-        textInput.setText("a3d85ac0-4268-bb12-a628-f1e13a4988d8")
-
         binding.buyButton.setOnSingleClickListener {
             activity.startService(TransactionService.getNewIntent(activity))
             onPaymentWithTokenButtonClick(activity)
